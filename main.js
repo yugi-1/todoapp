9e0b69484ccb691a1fd20b315be5f5e2700e1e61
Yulissa Gil <yulissa.gil9759@stu.mtec.edu>
2023-08-31 18:06:07 -0600

    finished adding todo function and delete function

--- main.js
+++ main.js
@@ -5,3 +5,26 @@ const data = {
     category: "School",
     date: "8-30-23"
 }
+
+let placement = document.getElementById('mainList');
+function addTodo() {
+    let inputVal = document.getElementById('mainValue').value;
+    let inputValTwo = document.getElementById('dateValue').value;
+    let inputValThree = document.getElementById('categoryValue').value;
+    let create = document.createElement("LI"); 
+    create.setAttribute( "class","list-group-item")
+    create.innerHTML = `${inputVal}` + `${inputValTwo}` + `${inputValThree}` + "<span class='closeBtn'>\u00D7</span>";
+    placement.appendChild(create); 
+
+    let close = document.getElementsByClassName(`closeBtn`);
+
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            let parent = this.parentElement;
+            parent.style.display = "none";
+          }
+    }
+}
+document.getElementById('mainCreate').addEventListener("click", addTodo);
+
+
