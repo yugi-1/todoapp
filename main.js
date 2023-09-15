ed2eb828e6a8be0bb377093229d98e6409160e69
Yulissa Gil <yulissa.gil9759@stu.mtec.edu>
2023-09-14 19:34:23 -0600

    decoration and most basic functions

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-const data = [];
+// const todos = {
+//     task: "taskname",
+//     category: "categoryname",
+//     date: "dateadded",
+//     id: 0
+// };
 
-const deleted = [];
+const todos=[];
+const deleted =[];
+
+
 let placement = document.getElementById('mainList');
 
 function addTodo() {
@@ -8,21 +16,39 @@ function addTodo() {
     let inputValTwo = document.getElementById('dateValue').value;
     let inputValThree = document.getElementById('categoryValue').value;
     let create = document.createElement("LI"); 
-    create.setAttribute( "class","list-group-item")
-    create.innerHTML = `<span contenteditable='true' >${inputVal}</span> ` + `<span>${inputValTwo}</span> ` + `${inputValThree}` + "<span class='closeBtn'>\u00D7</span>";
+    create.setAttribute( "class","list-group-item");
+    create.innerHTML = `<span contenteditable='true'>${inputVal}</span> ` + `<span>${inputValTwo}</span> ` + `${inputValThree}` + "<span class='closeBtn'>\u00D7</span>";
     placement.appendChild(create); 
-    
+
+    const createData = {
+        name: inputVal,
+        date: inputValTwo,
+        category: inputValThree
+    }
+
+
+    todos.push(createData);
+    console.log(todos);
+
     let close = document.getElementsByClassName(`closeBtn`);
 
     for (let i = 0; i < close.length; i++) {
         close[i].onclick = function() {
             let parent = this.parentElement;
             parent.style.display = "none";
+            let selector = todos[i];
+            todos.splice(selector);
+            console.log(selector)
+            console.log(todos);
+            // let index = todos.indexOf(parent);
+            // let x = todos.splice(index, 1);
+            // console.log(`${todos}`);
+            // console.log(`${x}`)
           }
     }
+
+    
 }
+
 document.getElementById('mainCreate').addEventListener("click", addTodo);
 
-function todos() {
-    document.getElementById("content").innerHTML = "<input type='text' id='todoAdd' placeholder='To Do...'><input type='submit' id='todoB' value='Add To Do' onclick='todoList()'>";
-  } 
