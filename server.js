db2c572f89effb45082e24462460051c51808a1e
Yulissa Gil <yulissa.gil9759@stu.mtec.edu>
2023-11-29 21:34:54 -0700

    starting api stuff

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('client'));
 
+
 app.listen(port, () => {
     console.log(`this is ${port}`);
 });
