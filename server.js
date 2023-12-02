61596312b8dc57d1850bfa55b08f54441b76be15
Yulissa Gil <yulissa.gil9759@stu.mtec.edu>
2023-12-01 17:51:34 -0700

    setup for apis

--- server.js
+++ server.js
@@ -6,7 +6,30 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('client'));
 
+app.get('/api/todos', (req, res) => {
+    res.send('todos');
+})
+
+app.post('/api/posttodos', (req, res) => {
+    res.send('post todos');
+})
+
+
+app.get('/api/todoscat', (req, res) => {
+    res.send('get all todos for category');
+})
+
+app.get('/api/categories', (req, res) => {
+    res.send('get categories');
+})
+
+app.post('/api/postcat', (req, res) => {
+    res.send('post categories');
+})
+
+
 
 app.listen(port, () => {
     console.log(`this is ${port}`);
 });
+
