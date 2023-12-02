20711d7f4add50e75f79298cc2a66bf496add010
Yulissa Gil <yulissa.gil9759@stu.mtec.edu>
2023-12-01 18:10:46 -0700

    setting up more apis

--- server.js
+++ server.js
@@ -8,28 +8,42 @@ app.use(express.static('client'));
 
 app.get('/api/todos', (req, res) => {
     res.send('todos');
-})
+});
 
 app.post('/api/posttodos', (req, res) => {
     res.send('post todos');
-})
+});
 
+app.put('/api/puttodos', (req, res) => {
+    res.send('put todos');
+});
+
+app.delete('/api/deletetodos', (req, res) => {
+    res.send('delete todos');
+});
 
 app.get('/api/todoscat', (req, res) => {
     res.send('get all todos for category');
-})
+});
 
 app.get('/api/categories', (req, res) => {
     res.send('get categories');
-})
+});
 
 app.post('/api/postcat', (req, res) => {
     res.send('post categories');
-})
+});
 
+app.put('/api/putcat', (req, res) => {
+    res.send('put categories');
+});
+
+app.delete('/api/deletecat', (req, res) => {
+    res.send('delete categories');
+});
 
 
 app.listen(port, () => {
     console.log(`this is ${port}`);
-});
+});;
 
